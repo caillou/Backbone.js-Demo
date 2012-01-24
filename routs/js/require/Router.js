7ad7e74a8fc60106ddd3ac8c875278de9494e24c
Pierre Spring <pierre@nelm.io>
2012-01-24 15:58:30 +0100

    initial commit

---
+++ routs/js/require/Router.js
@@ -0,0 +1,54 @@
+/*jslint white: false, es5: false */
+/*global define: true, Backbone: true, _: true */
+
+define([], function () {
+    
+    var displaySomething, links, linksArray;
+    
+    linksArray = [
+        'q?:backbone',
+        'show/42',
+        'search/pierre/Zurich',
+        'download/avatar/pierre/large'
+    ];
+    
+    links = $('<ul/>');
+    
+    _.each(linksArray, function (fragment) {
+        console.log(this, arguments);
+        fragment = '#' + fragment;
+        links.append('<li><a/></li>')
+            .find('a:last')
+            .attr('href', fragment)
+            .text(fragment);
+    });
+
+    displaySomething = function (displayString) {
+        return function () {
+            $('body').html('<h1/>')
+                .append(links)
+                .find('h1')
+                .text(displayString);
+            console.log(arguments);
+        };
+    };
+
+    return Backbone.Router.extend({
+
+        routes: {
+            '': 'index',
+            'q?:query': 'query',
+            'show/:id': 'show',
+            'search/:who/:where': 'search',
+            'download/*path': 'download'
+            
+        },
+
+        index: displaySomething('index'),
+        query: displaySomething('query'),
+        show: displaySomething('show'),
+        search: displaySomething('search'),
+        download: displaySomething('download')
+
+    });
+});
---
+++ routs/js/require/app.js
@@ -0,0 +1,7 @@
+/*jslint white: false, es5: false */
+/*global Backbone: true */
+
+require([ "Router" ], function (Router) {
+    new Router();
+    Backbone.history.start();
+});
