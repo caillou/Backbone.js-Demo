f3f765c99b7326115362493142e3c08061f16c5c
Pierre Spring <pierre@nelm.io>
2012-01-24 18:43:44 +0100

    adds local storage

--- 2 Comments/js/require/collections/Comment.js
+++ 2 Comments/js/require/collections/Comment.js
@@ -1,11 +1,12 @@
 /*jslint white: false, es5: false */
-/*global define: true, Backbone: true */
+/*global define: true, Backbone: true, Store: true */
 
 define(
     ['models/Comment'],
     function (CommentModel) {
         return Backbone.Collection.extend({
             model: CommentModel,
+            localStorage: new Store("todos"),
             initialize: function (options) {
             }
         });
