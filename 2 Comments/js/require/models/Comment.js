09849ca1b75b5aa93e8a80121cace74eee0d90cf
Pierre Spring <pierre@nelm.io>
2012-01-25 02:24:20 +0100

    adds the date and avatar to new comments

--- 2 Comments/js/require/models/Comment.js
+++ 2 Comments/js/require/models/Comment.js
@@ -1,11 +1,24 @@
 /*jslint white: false, es5: false */
-/*global define: true, Backbone: true, Store: true */
+/*global define: true, Backbone: true, Store: true, moment: true, md5: true */
 
 define(
     [], 
     function () {
         return Backbone.Model.extend({
-            localStorage: new Store("comment")
+            localStorage: new Store("comment"),
+            initialize: function (options) {
+                if (!options.date) {
+                    this.set({'date': moment().toString()});
+                }
+                if (!options.avatarUrl) {
+                    this.set({
+                        avatarUrl: 'https://secure.gravatar.com/avatar/' + md5(this.get('email'))
+                    });
+                }
+            },
+            getDisplayDate: function () {
+                return moment(this.get('date')).fromNow();
+            }
         });
     }
 );
